43150c6a3515c803767cde746ff0022087000779
you name <you@domain.com>
2022-04-10 15:49:50 +0430

    abolfazlmarzban

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - project1',
+    title: 'project1',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/styles.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [ '@/plugins/filters' ],
+  router:{
+    middleware: ['auth']
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyB6jidi171BN0OTN1COZlQ4TcWZ3L4sXKw",
+          authDomain: "project1-4296d.firebaseapp.com",
+          projectId: "project1-4296d",
+          storageBucket: "project1-4296d.appspot.com",
+          messagingSenderId: "1071473980988",
+          appId: "1:1071473980988:web:d29ef5d80ffe2aac21f539"
+        },
+        services: {
+          auth: {
+            persistence: 'local', // default
+            initialize: {
+              onAuthStateChangedAction: 'onAuthStateChangedAction',
+              subscribeManually: false
+            },
+            ssr: false,
+          },
+          firestore: true,
+          storage: true
+        }
+      }
+    ]
+  ],
+
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/auth/signout.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>Sign Out</h1>
+</template>
+
+<script>
+export default {
+async AsyncData(){
+    $nuxt.$fire.$auth.signOut()
+}
+}
+</script>
+
+<style>
+
+</style>
